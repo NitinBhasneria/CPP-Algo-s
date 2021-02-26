4607993950b2a5ac0d5e51cb6065c402492b1665
NitinBhasneria <nitinbhasneria360@gmail.com>
2021-02-26 21:03:53 +0530

    some

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,47 +1,56 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
+bool possible(int n, int num){
+	// cout<<"chla";
+	// cout<<"possibibltyu "<<num<<endl;
+	if(num==0)
+		return false;
+	int sharma=0, singh=0, balls=n;
+	while(n>0){
+		if(n-num<0){
+			sharma+=n;
+			break;
+		}
+		sharma+=num;
+		n-=num;
+		// cout<<sharma<<" "<<n;
+		singh+=(n/10);
+		n-=(n/10);
+		// cout<<" "<<singh<<" "<<n<<endl;
+	}
+	// cout<<sharma<<" "<<singh<<endl;
+	if((sharma*2)>=(balls))
+		return true;
+	return false;
+}
 
-int binarySearch(int *input, int n, int val)
-{
-    //Write your code here
-    int start =0, end = n-1;
-    int mid=(start+end)/2;
-    while(mid<end){
-        cout<<start<<" "<<mid<<" "<<end<<" "<<endl;
-        if(input[mid]<val)
-            start = mid;
-        else if(input[mid]>val)
-            end=mid;
-        else
-            return mid;
-        mid=(start+end)/2;
+int sol(int start, int end, int ans, int n){
+    if(start>end){
+		// cout<<"exited"<<" "<<start<<" "<<end<<" "<<ans<<endl;
+		return ans;
+	}
+	// cout<<start<<" "<<end<<" "<<ans<<endl;
+    int mid = (start+end)/2;
+    if(possible(n,mid)){
+        return sol(start,mid-1,mid, n);
     }
-    return -1;
+    else
+        return sol(mid+1,end,ans, n);
+        
+}
+
+int find_solution(int n){
+    int start=0,end=n,ans=n;
+    return sol(start,end,ans, n);
 }
 
 int main()
 {
-
-	int size;
-	cin >> size;
-	int *input = new int[size];
-
-	for(int i = 0; i < size; ++i)
-	{
-		cin >> input[i];
-	}
-	
-	int t;
-	cin >> t;
-
-	while (t--)
-	{
-		int val;
-		cin >> val;
-		cout << binarySearch(input, size, val) << endl;
-	}
-	
-	delete [] input;
+    int n;
+	cin>>n;
+	// if(possible(43,1))
+	// 	cout<<"true";
+    cout<<find_solution(n)<<endl; 
 	return 0;
 }
