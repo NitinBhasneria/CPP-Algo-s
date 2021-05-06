9ec2fb8dbb8823a6fb8f940aef21aea338bf5d9a
NitinBhasneria <nitinbhasneria360@gmail.com>
2021-05-06 07:13:59 +0530

    Added Question

--- Fenwick_tree_or_BIT/KQUERY.cpp
+++ Fenwick_tree_or_BIT/KQUERY.cpp
@@ -26,25 +26,79 @@ Sample Output
 */
 #include<bits/stdc++.h>
 using namespace std;
-
-struct node {
+#define MAX 30002
+struct input {
     int value, index;
+} arr[MAX];
+struct node {
+    int left, right, k, index;
 } query[200005];
+
+bool compare(node a, node b) {
+    return a.k>b.k;
+}
+bool compare2(input a, input b) {
+    return a.value>b.value;
+}
+int bit[MAX];
+
+void update(int i,int n)
+{
+    for(;i<=n;i+=(i&(-i))){
+        bit[i]++;
+    }
+}
+
+int output(int index) {
+    int res = 0;
+    for(;index>0;index-= index & (-index)){
+        res += bit[index];
+    }
+    return res;
+}
 
 int main() {
     int n;
     cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+    for(int i=1;i<=n;i++){
+        cin>>arr[i].value;
+        arr[i].index = i;
     }
     int q;
     cin>>q;
     for(int i=0;i<q;i++){
-        cin>>query[i].value;
+        cin>>query[i].left>>query[i].right>>query[i].k;
         query[i].index = i;
     }
 
+    sort(query, query+q,compare);
+    arr[0].value = INT_MAX;
+    arr[0].index = INT_MAX;
+    
+    sort(arr, arr+n+1, compare2);
+    
+    int j=1;
+    int* ans = new int[q]();
+    for(int i=0;i<q;i++) {
+        while(j<=n) {
+            if(arr[j].value > query[i].k){
+                // cout<<query[i].k<<" "<<arr[j].value<<"\n";
+                // cout<<"added"<<arr[j].value<<endl;
+                update(arr[j].index, n);
+                j++;
+            } else{
+                break;
+            }
+        }
+        int temp = output(query[i].right) - output(query[i].left-1);
+        // cout<<query[i].left<<" "<<query[i].right<<" "<<temp<<endl;
+        ans[query[i].index] = temp;
+    }
+    for(int i=0;i<q;i++) {
+        cout<< ans[i] <<"\n";
+    }
+
+    delete[] ans;
     return 0;
 }
 
